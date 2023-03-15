1b2f68393eed0fcf8c7f92884d470f548b2a835e
PoornaCDW <125631705+PoornaCDW@users.noreply.github.com>
2023-03-15 21:43:37 +0530

    Exercise 11 commit!

---
+++ calculator-js/js/calculator.js
@@ -0,0 +1,27 @@
+//Calculator
+
+let calculator = {
+    add(number1, number2) {
+        return parseFloat(number1)+parseFloat(number2);
+    },
+    subtract(number1, number2) {
+        return parseFloat(number1)-parseFloat(number2);
+    },
+    multiply(number1, number2) {
+        return parseFloat(number1)*parseFloat(number2);
+    },
+    divide(number1, number2) {
+        if(number2 == 0) {
+            return "Cannot divide the number by 0.";
+        } else {   
+            return parseFloat(number1)/parseFloat(number2);
+        }
+    }
+}
+
+//Calculator call
+console.log("Sum: ", calculator.add(2, 5));
+console.log("Difference: ", calculator.subtract("78", 28));
+console.log("Product: ", calculator.multiply(45, "2.1"));
+console.log("Quotient: ", calculator.divide(42, 0));
+console.log("Quotient: ", calculator.divide(42, 10));
