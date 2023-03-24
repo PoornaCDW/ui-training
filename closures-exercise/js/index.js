c152ec1104efd41401e2a1e1dbfe82f4376e0a89
PoornaCDW <125631705+PoornaCDW@users.noreply.github.com>
2023-03-24 10:08:02 +0530

    Exercise 16 #1

---
+++ closures-exercise/js/index.js
@@ -0,0 +1,36 @@
+(function() {
+    let bankAccounts = [
+        {
+            accountNumber: 5940,
+            cardNumber: 1234123412341234,
+            atmPin: 3109,
+            accountBalance: 14500
+        },
+        {
+            accountNumber: 2905,
+            cardNumber: 1234567812345678,
+            atmPin: 0931,
+            accountBalance: 1000
+        },
+        {
+            accountNumber: 6790,
+            cardNumber: 123400001234000,
+            atmPin: 8922,
+            accountBalance: 9000
+        },
+        {
+            accountNumber: 4560,
+            cardNumber: 0987098709870987,
+            atmPin: 7765,
+            accountBalance: 0
+        },
+        {
+            accountNumber: 5790,
+            cardNumber: 6789678967896789,
+            atmPin: 0000,
+            accountBalance: 87000
+        }
+    ]
+
+    console.log(bankAccounts);
+})();
